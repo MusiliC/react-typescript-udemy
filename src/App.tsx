3e496f6a4d913d8c97f7c38d32f3815dc257a84a
brian.musili <brian.musili@systechafrica.com>
2024-12-28 17:23:15 +0300

    Wrapper components with forward refs

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,21 @@
 import Input from "./components/Input";
-import Form from "./components/Form";
+import Form, { FormHandle } from "./components/Form";
 import Button from "./components/Button";
+import { useRef } from "react";
 
 function App() {
+
+  const customForm = useRef<FormHandle>(null)
+
   function handleSave(data: unknown) {
     const extractedData = data as { name: string; age: string };
     console.log(extractedData);
+    customForm.current?.clear()
   }
 
   return (
     <main>
-      <Form onSave={handleSave}>
+      <Form onSave={handleSave} ref={customForm}>
         <Input label="Name" id="name" type="text" />
         <Input label="Age" id="age" type="number" />
         <p>
--- src/components/Form.tsx
+++ src/components/Form.tsx
@@ -1,10 +1,26 @@
-import { type FormEvent, type ComponentPropsWithoutRef } from "react";
+import { type FormEvent, type ComponentPropsWithoutRef, useRef, useImperativeHandle, forwardRef } from "react";
 
 type FormProps = ComponentPropsWithoutRef<"form"> & {
   onSave: (values: unknown) => void;
 };
 
-function Form({ onSave, children, ...otherProps }: FormProps) {
+export type FormHandle = {
+  clear: () => void
+}
+
+const Form = forwardRef <FormHandle, FormProps>(function Form({ onSave, children, ...otherProps }, ref) {
+
+  const form = useRef<HTMLFormElement>(null)
+
+  useImperativeHandle(ref, () => {
+    return{
+      clear(){
+        form.current?.reset()
+      }
+    }
+  })
+
+
   function handleSubmit(event: FormEvent<HTMLFormElement>) {
     event.preventDefault();
 
@@ -13,13 +29,14 @@ function Form({ onSave, children, ...otherProps }: FormProps) {
     const data = Object.fromEntries(formData);
 
     onSave(data);
+
   }
 
   return (
-    <form onSubmit={handleSubmit} {...otherProps}>
+    <form onSubmit={handleSubmit} {...otherProps} ref={form}>
       {children}
     </form>
   );
-}
+})
 
 export default Form;
