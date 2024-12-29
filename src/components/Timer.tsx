bb0628181b1e2b3393c176647ae32598252025ec
brian.musili <brian.musili@systechafrica.com>
2024-12-29 14:40:54 +0300

    useEffect and useState in typescript

--- src/components/Timer.tsx
+++ src/components/Timer.tsx
@@ -1,15 +1,47 @@
-import { Timer as TimeProps} from '../store/TimersContext.tsx';
-import Container from './UI/Container.tsx';
+import { useEffect, useRef, useState } from "react";
+import {
+  Timer as TimeProps,
+  useTimersContext,
+} from "../store/TimersContext.tsx";
+import Container from "./UI/Container.tsx";
 
+export default function Timer({ name, duration }: TimeProps) {
+  const { isRunning } = useTimersContext();
 
+  const intervalTimer = useRef<number | null>(null);
 
+  const [remainingTime, setRemainingTime] = useState(duration * 1000);
 
-export default function Timer({name, duration}: TimeProps) {
+  if (remainingTime <= 0 && intervalTimer.current) {
+    clearInterval(intervalTimer.current);
+  }
+
+  useEffect(() => {
+    let timer: number;
+
+    if (isRunning) {
+      timer = setInterval(function () {
+        setRemainingTime((prevTime) => prevTime - 50);
+      }, 50);
+
+      intervalTimer.current = timer;
+    } else if (intervalTimer.current) {
+      clearInterval(intervalTimer.current);
+    }
+
+    return () => clearInterval(timer);
+  }, [isRunning]);
+
+  const formattedRemainingTime = (remainingTime / 1000).toFixed(2);
+
   return (
     <Container as="article">
       <h2>TODO: TIMER NAME</h2>
       <h2>{name}</h2>
-      <p>{duration}</p>
+      <p>
+        <progress max={duration * 1000} value={remainingTime} />
+      </p>
+      <p>{formattedRemainingTime}</p>
     </Container>
   );
 }
