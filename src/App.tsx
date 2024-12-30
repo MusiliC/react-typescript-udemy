eb0821a0a72ec8fd870d146b28b8f40ee3dc64da
brian.musili <brian.musili@systechafrica.com>
2024-12-30 07:03:48 +0300

    fetch with typescript

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,70 @@
-import AddTimer from './components/AddTimer.tsx';
-import Header from './components/Header.tsx';
-import Timers from './components/Timers.tsx';
-import TimersContextProvider from './store/TimersContext.tsx';
+import { ReactNode, useEffect, useState } from "react";
+import { get } from "./components/http";
+import BlogPosts, { BlogPost } from "./components/BlogPosts";
+import fetchingImage from "./assets/data-fetching.png";
+import ErrorMessage from "./components/ErrorMessage";
+
+type RawDataBlogPost = {
+  id: number;
+  userId: number;
+  title: string;
+  body: string;
+};
 
 function App() {
+  const [fetchedPosts, setFetchedPosts] = useState<BlogPost[]>();
+  const [isFetching, setIsFetching] = useState(false);
+  const [error, setError] = useState<string>();
+
+  useEffect(() => {
+    async function fetchPosts() {
+      setIsFetching(true);
+
+      try {
+        const data = (await get(
+          "https://jsonplaceholder.typicode.com/posts"
+        )) as RawDataBlogPost[];
+
+        const blogPosts: BlogPost[] = data.map((rawPost) => {
+          return {
+            id: rawPost.id,
+            title: rawPost.title,
+            text: rawPost.body,
+          };
+        });
+
+        setFetchedPosts(blogPosts);
+      } catch (error) {
+        if (error instanceof Error) {
+          setError(error.message);
+        }
+      }
+
+      setIsFetching(false);
+    }
+    fetchPosts();
+  }, []);
+
+  let content: ReactNode;
+
+  if (fetchedPosts) {
+    content = <BlogPosts posts={fetchedPosts} />;
+  }
+
+  if (isFetching) {
+    <p id="loading-fallback">Loading...</p>;
+  }
+
+  if (error) {
+    content = <ErrorMessage text={error} />;
+  }
+
   return (
-    <TimersContextProvider>
-      <Header />
-      <main>
-        <AddTimer />
-        <Timers />
-      </main>
-    </TimersContextProvider>
+    <main>
+      <img src={fetchingImage} alt="Cee" />
+
+      {content}
+    </main>
   );
 }
 
