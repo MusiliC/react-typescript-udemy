ea877c2236bcd4335ecb232205825497ecf7a127
brian.musili <brian.musili@systechafrica.com>
2024-12-30 07:47:53 +0300

    create redux slice with typescript

--- src/App.tsx
+++ src/App.tsx
@@ -1,70 +1,20 @@
-import { ReactNode, useEffect, useState } from "react";
-import { get } from "./components/http";
-import BlogPosts, { BlogPost } from "./components/BlogPosts";
-import fetchingImage from "./assets/data-fetching.png";
-import ErrorMessage from "./components/ErrorMessage";
-
-type RawDataBlogPost = {
-  id: number;
-  userId: number;
-  title: string;
-  body: string;
-};
+import Header from './components/Header.tsx';
+import Shop from './components/Shop.tsx';
+import Product from './components/Product.tsx';
+import { DUMMY_PRODUCTS } from './dummy-products.ts';
 
 function App() {
-  const [fetchedPosts, setFetchedPosts] = useState<BlogPost[]>();
-  const [isFetching, setIsFetching] = useState(false);
-  const [error, setError] = useState<string>();
-
-  useEffect(() => {
-    async function fetchPosts() {
-      setIsFetching(true);
-
-      try {
-        const data = (await get(
-          "https://jsonplaceholder.typicode.com/posts"
-        )) as RawDataBlogPost[];
-
-        const blogPosts: BlogPost[] = data.map((rawPost) => {
-          return {
-            id: rawPost.id,
-            title: rawPost.title,
-            text: rawPost.body,
-          };
-        });
-
-        setFetchedPosts(blogPosts);
-      } catch (error) {
-        if (error instanceof Error) {
-          setError(error.message);
-        }
-      }
-
-      setIsFetching(false);
-    }
-    fetchPosts();
-  }, []);
-
-  let content: ReactNode;
-
-  if (fetchedPosts) {
-    content = <BlogPosts posts={fetchedPosts} />;
-  }
-
-  if (isFetching) {
-    <p id="loading-fallback">Loading...</p>;
-  }
-
-  if (error) {
-    content = <ErrorMessage text={error} />;
-  }
-
   return (
-    <main>
-      <img src={fetchingImage} alt="Cee" />
-
-      {content}
-    </main>
+    <>
+      <Header />
+      <Shop>
+        {DUMMY_PRODUCTS.map((product) => (
+          <li key={product.id}>
+            <Product {...product} />
+          </li>
+        ))}
+      </Shop>
+    </>
   );
 }
 
